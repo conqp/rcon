531a7acac7ccbfffdb8fe19d5ba9dc5cf70fb686
Richard Neumann <mail@richard-neumann.de>
2020-12-31 15:12:45 +0100

    Added suppot for config file on Windows systems.

--- rcon/config.py
+++ rcon/config.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 from configparser import ConfigParser, SectionProxy
 from logging import getLogger
+from os import getenv, name
 from pathlib import Path
 from typing import Dict, Iterator, NamedTuple, Tuple
 
@@ -13,7 +14,14 @@ __all__ = ['CONFIG_FILE', 'LOG_FORMAT', 'Config', 'servers']
 
 
 CONFIG = ConfigParser()
-CONFIG_FILE = Path('/etc/rcon.conf')
+
+if name == 'posix':
+    CONFIG_FILE = Path('/etc/rcon.conf')
+elif name == 'nt':
+    CONFIG_FILE = Path(getenv('LOCALAPPDATA')).joinpath('rcon.conf')
+else:
+    raise NotImplementedError(f'Unsupported operating system: {name}')
+
 LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
 LOGGER = getLogger('RCON Config')
 
