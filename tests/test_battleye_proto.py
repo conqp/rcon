b7ca56b086d4c3a7c154c7f7c1cf71c1f32954ce
Richard Neumann <r.neumann@homeinfo.de>
2022-02-14 11:45:46 +0100

    Add unit tests for BattlEye

---
+++ tests/test_battleye_proto.py
@@ -0,0 +1,17 @@
+"""Test the BattlEye protocol."""
+
+from unittest import TestCase
+
+from rcon.battleye.proto import Header
+
+
+HEADER = Header(920575337, 0x00)
+BYTES = b'BEi\xdd\xde6\xff\x00'
+
+
+class TestHeader(TestCase):
+    """Test header object."""
+
+    def test_header_from_bytes(self):
+        """Tests header object parsing."""
+        self.assertEqual(Header.from_bytes(BYTES), HEADER)
